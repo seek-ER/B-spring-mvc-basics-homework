30902b85a617890f991a0264cbfaa0ca6580feed
Anson <1072476988@qq.com>
2020-11-04 17:08:20 +0800

    [Lingzhen Kong] feat: add code on error body.

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/component/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/component/GlobalExceptionHandler.java
@@ -8,19 +8,21 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Objects;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Error> handle(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getFieldError().getDefaultMessage();
-        Error error = new Error(message);
+        String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        Error error = new Error(HttpStatus.BAD_REQUEST.value(), message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(UserExistException.class)
     public ResponseEntity<Error> handle(UserExistException e) {
         String message = e.getMessage();
-        Error error = new Error(message);
+        Error error = new Error(HttpStatus.BAD_REQUEST.value(), message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/error/Error.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/error/Error.java
@@ -8,5 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Error {
+    private int code;
     private String message;
 }
